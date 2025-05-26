096089e3ae6c179c80cc94a1d7b631b22bf00188
tamaraalsaad <tamaraalsaad@yahoo.com>
2025-05-26 10:25:54 +0200

    add fail

---
+++ src/Inlämning/testTextCounter.java
@@ -0,0 +1,48 @@
+package Inlämning;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class testTextCounter {
+
+    @Test
+    public void testSingleLine(){
+
+        textCounter counter= new textCounter();
+        counter.addLine("Hello");
+        int actual=counter.getLineCount();
+        int expected=1;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testMultipleLines(){
+        textCounter counter= new textCounter();
+        counter.addLine("Hello");
+        counter.addLine("My name is");
+        int actual=counter.getLineCount();
+        int expected=2;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testCharCount(){
+        textCounter counter= new textCounter();
+        counter.addLine("Hello");
+        int actual=counter.getCharCount();
+        int expected=5;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testAddCommas(){
+        textCounter counter= new textCounter();
+        counter.addLine("Hello");
+        counter.addLine(",");
+        int actual=counter.getCharCount();
+        int expected=6;
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+}
+
+
---
+++ src/Inlämning/textRead.java
@@ -0,0 +1,26 @@
+package Inlämning;
+
+import java.util.Scanner;
+
+
+    public class textRead {
+        public static void main(String[] args) {
+            Scanner scan=new Scanner(System.in);
+            textCounter counter= new textCounter();
+            while (true ) {
+
+                String line = scan.nextLine();
+                if (line.equals("stop")) {
+                    break;
+
+                }
+                counter.addLine(line);
+            }
+
+            System.out.println("Namber of lines:"+counter.getLineCount());
+            System.out.println("Naumder of characters:"+counter.getCharCount());
+
+        }
+    }
+
+
